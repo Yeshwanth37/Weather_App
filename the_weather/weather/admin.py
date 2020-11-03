b549adde4f4eeb32a0472abd7b4b17755effe420
Yeshwanth <yeshwanth.nellikanti37@gmail.com>
2020-11-03 10:20:21 -0600

    updated views.py file

--- the_weather/weather/admin.py
+++ the_weather/weather/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import City
 
-# Register your models here.
+# admin.site.register(City)
+
+@admin.register(City)
+class City(admin.ModelAdmin):
+    list_display = ['name']
